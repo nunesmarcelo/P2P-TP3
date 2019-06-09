715cbee26f984f5524b6e49c2d99964bf2dad028
Marcelo Nunes <marcelonunes3698@gmail.com>
2019-06-09 18:38:11 -0300

    avanços atuais

--- TP3client.py
+++ TP3client.py
@@ -5,36 +5,123 @@
 #   Wanderson Sena
 
 
-import socket
-import sys
+import socket , sys , os , struct , select
 
-messages = [ 'This is the message. ',
-             'It will be sent ',
-             'in parts.',
-             ]
-server_address = ('localhost', 10000)
+class TP3client:
+    def __init__(self):
+        if(len(sys.argv) < 3): 
+            os._exit()
 
-# Create a TCP/IP socket
-socks = [ socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-          socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-          ]
+        self.port = sys.argv[1]
+        self.serverIp = sys.argv[2]
+        self.socketsList = { }
+        self.socketsList['stdin'] = sys.stdin
+        self.nseq = 0
 
-# Connect the socket to the port where the server is listening
-print ('connecting to %s port %s' % server_address)
-for s in socks:
-    s.connect(server_address)
+        # Create accept socket and the socket to communicate with the servent.
+        self.createInitialSockets()
 
-for message in messages:
+        # Start to listen the user commands and the answer received from the servents
+        self.listenCommandsAndAnswers()
 
-    # Send messages on both sockets
-    for s in socks:
-        print ('%s: sending "%s"' % (s.getsockname(), message))
-        s.send( str.encode( message) )
+    def createInitialSockets(self):
+            try:
+                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a TCP/IP socket
+                client.setblocking(0)
+                # Bind the socket to the port
+                myaddress = ('', int(self.port))
+                print (  'starting up on %s port %s' % myaddress)
+                client.bind(myaddress)
+                # Listen for incoming connections - 1000 =  high arbitrary value. =)
+                client.listen(1000)
 
-    # Read responses on both sockets
-    for s in socks:
-        data = bytes.decode( s.recv(1024) )
-        print ('%s: received "%s"' % (s.getsockname(), data))
-        if not data:
-            print ('closing socket', s.getsockname())
-            s.close()
+                self.socketsList['0'] = client
+            except socket.error as e:
+                print ('Could not bind the socket connection ' , e)
+                os._exit(1)
+
+            # Servent socket
+            try:
+                servent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# Create a TCP/IP socket
+                servent.connect( ( self.serverIp.split(':')[0] , int( self.serverIp.split(':')[1] ) ) ) # connect to servent
+
+                # > big-endian, padrão do TCP/IP
+                # h short integer - 2 bytes
+                messageId = struct.pack('>h', 4) + struct.pack('>h', int(self.port))   
+
+                # messageId is used to warn the other side
+                servent.send( messageId )
+                # Sockets from which we expect to read
+                self.socketsList[ self.serverIp]  = servent
+            except ConnectionRefusedError:
+                print("Connection failure. " , self.serverIp)
+            except socket.error:
+                print ('Could not connect to chat server ')
+                os._exit(1)
+            
+    def listenCommandsAndAnswers(self):
+        try:
+            while(1):
+                # Get the list sockets which are readable
+                read_sockets, write_sockets, error_sockets = select.select( [ self.socketsList[sockeet] for sockeet in self.socketsList ], [], [])
+                for sock in read_sockets:
+                    # If is a new connection
+                    if sock is self.socketsList['stdin']:
+                        print(">>  " , end=" ")
+                        command = sock.readline()
+                        print(command)
+
+                        if (command[0] == "?" and " " in command):
+                            # read command and remove \n or \0 to avoid mistakes
+                            searchedKey = command[2:].replace('\0',"").replace('\n',"")
+
+                            messageKEYREQ = struct.pack('>h', 5) + struct.pack('>i', self.nseq) + struct.pack('>h', len(searchedKey)) 
+                            messageKEYREQ += str.encode(searchedKey)
+                            # Ask for the value in the key entered by the user
+                            self.socketsList[self.serverIp].send( messageKEYREQ )
+
+                        elif(command == "Q"):
+                            raise KeyboardInterrupt
+                        else:
+                            print("Comando desconhecido")
+
+                        #self.socketsList[ self.serverIp ].send()
+                    elif sock is self.socketsList['0']:
+                        # A "readable" server socket is ready to accept a connection
+                        connection, client_address = sock.accept()
+                        print ('new connection from: ', client_address)
+                        connection.setblocking(0)
+                        self.socketsList[client_address] = connection
+                    else:
+                        data = sock.recv(2) # receive message type
+                        if data: # Connection is open yet
+                            #               ID   KEYREQ TOPOREQ KEYFLOOD TOPOFLOOD RESP
+                            #Valor de tipo  4    5      6       7        8         9
+                            valueType = struct.unpack('>h', data)[0] 
+                            if valueType == 9:
+                                nseq = struct.pack('>i', sock.recv(2))[0]
+                                tamanho =  struct.pack('>h', sock.recv(2))[0]
+                                
+
+                                # Terminar de receber o RESP no cliente... servidor já pegou a key, achou pra ele, e está 
+                                #enviando de novo!!!! =)
+                                i=0
+                                 = ""
+                                while(i < tamanho):
+
+                                    i+=1
+                        else:
+                            raise KeyboardInterrupt
+        except KeyboardInterrupt:
+            print("Closing connections...")
+            for x in self.socketsList:
+                self.socketsList[x].close()
+            os._exit(0)
+
+if __name__ == "__main__":
+    try:
+        node = TP3client()
+    except KeyboardInterrupt:
+        for x in self.socketsList:
+                self.socketsList[x].close()
+        os._exit(0)
--- TP3node.py
+++ TP3node.py
@@ -11,26 +11,27 @@ class TP3node:
         if(len(sys.argv) < 3): 
             os._exit()
 
-        # { 'ip:port' : {'socket': <socket> , 'type': 'server/servent/client'} }
-        self.socketsList = {}
-        self.typesList = {}
+        try:
+            self.socketsList = {}
+            self.typesList = {}
 
-        # Read dict -> key value from file
-        self.readDb()
+            # Read dict -> key value from file
+            self.readDb()
 
-        # Create server socket before start listen
-        self.createGeneralSocket()
+            # Create server socket before start listen
+            self.createGeneralSocket()
 
-        # Read list of neighbors in argv
-        self.readInputNeighbors()
+            # Read list of neighbors in argv
+            self.readInputNeighbors()
 
-        # Create socket for listen connections (using select function)
-        self.startListenConnections()
+            # Create socket for listen connections (using select function)
+            self.startListenConnections()
 
-
-        print(self.socketsList)
-        print(self.typesList)
-        
+        except KeyboardInterrupt:
+            print("Removendo IPs...")
+            for x in self.socketsList:
+                self.socketsList[x].close() 
+            os._exit(1)
         
     def readDb(self):
         try:
@@ -46,92 +47,127 @@ class TP3node:
             #print(self.db)
         except AttributeError:
             print("File not found!")
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
         except Exception as e:
             print(e)
 
     def createGeneralSocket(self):
-        self.port = sys.argv[1]
+        try:
+            self.port = sys.argv[1]
 
-        # Create a TCP/IP socket
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.setblocking(0)
+            # Create a TCP/IP socket
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.setblocking(0)
 
-        # Bind the socket to the port
-        server_address = ('', int(self.port))
-        print (  'starting up on %s port %s' % server_address)
-        server.bind(server_address)
+            # Bind the socket to the port
+            server_address = ('', int(self.port))
+            print (  'starting up on %s port %s' % server_address)
+            server.bind(server_address)
 
-        # Listen for incoming connections - 1000 = arbitrary value. =)
-        server.listen(1000)
+            # Listen for incoming connections - 1000 = arbitrary value. =)
+            server.listen(1000)
 
-        # Sockets from which we expect to read
-        self.socketsList['0'] = server
-        self.typesList['0'] = 'server'
+            # Sockets from which we expect to read
+            self.socketsList['0'] = server
+            self.typesList['0'] = 'server'
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
      
     def readInputNeighbors(self):
-        addresses = sys.argv[3:]
-        for address in set(addresses):
-            conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                conn.connect( (address.split(':')[0] , int( address.split(':')[1]) ) ) 
+        try:
+            addresses = sys.argv[3:]
+            for address in set(addresses):
+                conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                try:
+                    conn.connect( (address.split(':')[0] , int( address.split(':')[1]) ) ) 
 
-                # > big-endian, padrão do TCP/IP
-                # h short integer - 2 bytes
-                messageId = struct.pack('>h', 4) + struct.pack('>h', 0)   
+                    # > big-endian, padrão do TCP/IP
+                    # h short integer - 2 bytes
+                    messageId = struct.pack('>h', 4) + struct.pack('>h', 0)   
 
-                # messageId is used to warn the other side
-                conn.send( messageId )
-                self.socketsList[ address ] = conn
-                self.typesList[ address ] = 'servent'
-            except ConnectionRefusedError:
-                print("Connection failure. " , address)
+                    # messageId is used to warn the other side
+                    conn.send( messageId )
+                    self.socketsList[ address ] = conn
+                    self.typesList[ address ] = 0
+                except ConnectionRefusedError:
+                    print("Connection failure. " , address)
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
 
     def startListenConnections(self):
-        while(self.socketsList):
-            # Get the list sockets which are readable
-            read_sockets, write_sockets, error_sockets = select.select( [ self.socketsList[sock] for sock in self.socketsList ], [], [])
-		
-            for sock in read_sockets:
-                # If is a new connection
-                if sock is self.socketsList['0']:
-                    # A "readable" server socket is ready to accept a connection
-                    connection, client_address = sock.accept()
-                    print ('new connection from: ', client_address)
-                    connection.setblocking(0)
-                    self.socketsList[client_address] = connection
-                # Else -> data coming
-                else:
-                    data = sock.recv(2) # receive message type
-                    
-                    if data:
-                        messageType = struct.unpack('>h', data)[0]
-                        print(messageType)
-                        if(messageType == 4):
-                            print("Chegou id!")
-                            portOrZero = struct.unpack('>h', sock.recv(2) )[0] # receive message type
-                            print("Port:" , portOrZero)
-                            if sock.getpeername() not in self.socketsList:
-                                print("não estava na lista!")
-                                os._exit(1)
-                            if portOrZero == 0:
-                                self.typesList[ sock.getpeername() ] = 'servent'
-                            else:
-                                self.typesList[ sock.getpeername() ] = 'client'
-                                # tratar casos de cliente, salvar porto para enviar dados.
-
-                        print (  'received "%s" from %s' % (data, sock.getpeername()))
-                        # Add output channel for response
-                        
-
+        try:
+            while(self.socketsList):
+                # Get the list of sockets which are readable
+                read_sockets, write_sockets, error_sockets = select.select( [ self.socketsList[sock] for sock in self.socketsList ], [], [])
+            
+                for sock in read_sockets:
+                    # If is a new connection
+                    if sock is self.socketsList['0']:
+                        # A "readable" server socket is ready to accept a connection
+                        connection, client_address = sock.accept()
+                        print ('new connection from: ', client_address)
+                        connection.setblocking(0)
+                        self.socketsList[client_address] = connection
+                    # Else -> data is coming
                     else:
-                        # Interpret empty result as closed connection
-                        print (  'closing', client_address, 'after reading no data')
-                        # Stop listening for input on the connection
-                        del self.socketsList[sock.getpeername()]
-                        sock.close()
+                        data = sock.recv(2)
+                        
+                        if data: # Connection is open yet
+                            #               ID   KEYREQ TOPOREQ KEYFLOOD TOPOFLOOD RESP
+                            #Valor de tipo  4    5      6       7        8         9
+                            valueType = struct.unpack('>h', data)[0] 
+
+                            if(valueType == 4):
+                                portOrZero = struct.unpack('>h', sock.recv(2) )[0] # receive message type
+                                print("Chegou mensagem tipo ID, porto:" , portOrZero)
+                                if sock.getpeername() not in self.socketsList:
+                                    print("não estava na lista!")
+                                    os._exit(1)
+                                self.typesList[ sock.getpeername() ] = portOrZero
+
+                            elif(valueType == 5):
+                                nseq = struct.unpack('>i', sock.recv(4) )[0] # unpack: > big-endian , i integer - 4 bytes
+                                tamanho = struct.unpack('>h' , sock.recv(2))[0] # unpack: > big-endian , h short integer - 2 bytes
+                                
+                                searchedKey = ""
+                                i=0
+                                while( i < tamanho):
+                                    searchedKey += bytes.decode( sock.recv(1) )
+                                    i+=1
+
+                                if(searchedKey in self.db.keys()):
+                                    # data = db value , ip = ip of sender , port = port that client gave me , nseq = came inside the package
+                                    self.replyToClient(self.db[searchedKey] , sock.getpeername()[0] , self.typesList[ sock.getpeername() ] , nseq)
+                        else:
+                            # Interpret empty result as closed connection
+                            print (  'closing ', sock.getpeername(), ' after reading no data')
+                            # Stop listening for input on the connection
+                            del self.socketsList[sock.getpeername()]
+                            sock.close()
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
+
+    def replyToClient(self , data , ip , port , nseq):
+        # +---- 2 ---+-- 4 -+--- 2 ---+----------\\---------------+
+        # | TIPO = 9 | NSEQ | TAMANHO | VALOR (at´e 400 carateres) |
+        # +----------+------+---------+----------\\---------------+
+        messageRESP= struct.pack('>h', 9) + struct.pack('>i', nseq) + struct.pack('>h', len(data)) 
+        messageRESP += str.encode( data )
+
+        # Temporary connection to send the message.
+        tempSocket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+        tempSocket.connect( ( ip, port ) )
+        tempSocket.send( messageKEYREQ )
+        tempSocket.close()
 
 
+# testsock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+# testsock.connect(( sock.getpeername()[0] , portOrZero ))
 
+# messageId = struct.pack('>h', 4) + struct.pack('>h', 0)   # pack : > big-endian , h short integer - 2 bytes
+# # messageId is used to warn the other side
+# testsock.send( messageId )
 
 
 
